61bf9f42d2a657da7dfd8c1a44e79351aef1b37b
PribylovNikita <54896261+PribylovNikita@users.noreply.github.com>
2020-12-30 13:23:19 +0300

    Add files via upload

---
+++ Pribylov/cw/src/Treap.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include "Treap.h"
+
+namespace treap {
+
+    TreapPtr build(const std::vector<int>& keys) {
+        Logger::instance().stream.setstate(std::ios_base::failbit);
+        std::cout.setstate(std::ios_base::failbit);
+
+        auto tr = std::make_shared<Treap>(keys[0]);
+        for (int i = 1; i < keys.size(); i++) {
+            insert(tr, keys[i]);
+        }
+
+        Logger::instance().stream.clear();
+        std::cout.clear();
+        return tr;
+    }
+
+    Treap::Treap(int key, int priority, TreapPtr left, TreapPtr right)
+            : key(key), priority(priority), left(left), right(right) {};
+
+    TreapPtr makeNode(int key) {
+        return std::make_shared<Treap>(key);
+    }
+
+    TreapPtr merge(const TreapPtr& L, const TreapPtr& R) {
+        if (L == nullptr) return R;
+        if (R == nullptr) return L;
+
+        TreapPtr merged = nullptr;
+        if (L->priority > R->priority) {
+            merged = merge(L->right, R);
+            merged = std::make_shared<Treap>(L->key, L->priority, L->left, merged);
+        } else {
+            merged = merge(L, R->left);
+            merged = std::make_shared<Treap>(R->key, R->priority, merged, R->right);
+        }
+        return merged;
+    }
+
+    TreapPair split(const TreapPtr& T, int key) {
+        if (T == nullptr) return TreapPair(nullptr, nullptr);
+
+        if (T->key <= key) {
+            TreapPair R = split(T->right, key);
+            TreapPtr L = std::make_shared<Treap>(T->key, T->priority, T->left, R.first);
+            return TreapPair(L, R.second);
+        } else {
+            TreapPair L = split(T->left, key);
+            TreapPtr R = std::make_shared<Treap>(T->key, T->priority, L.second, T->right);
+            return TreapPair(L.first, R);
+        }
+    }
+
+    TreapPtr insert(TreapPtr& T, int key) {
+        Logger::instance().log("Добавление элемента M с ключом " + std::to_string(key) + ":\n");
+        TreapPtr newNode = std::make_shared<Treap>(key);
+
+        TreapPair pair = split(T, key);
+        Logger::instance().log("Дерево T расщеплено по ключу " + std::to_string(key) + ":\n");
+        Logger::instance().log("Левая половинка L: \n");
+        printTree(pair.first, nullptr, false);
+        Logger::instance().log("Правая половинка R: \n");
+        printTree(pair.second, nullptr, false);
+
+        TreapPtr left = merge(pair.first, newNode);
+        Logger::instance().log("Левая половинка L сцеплена с элементом M с ключом " + std::to_string(key) + ":\n");
+        printTree(left, nullptr, false);
+
+        T = merge(left, pair.second);
+        Logger::instance().log("Получившееся дерево L+M сцеплено с правой половинкой R:\n");
+        printTree(T, nullptr, false);
+
+        Logger::instance().log("Добавление элемента завершено.\n");
+        return T;
+    }
+
+    TreapPtr remove(TreapPtr& T, int key) {
+        Logger::instance().log("Удаление элемента с ключом " + std::to_string(key) + ":\n");
+
+        TreapPair pair = split(T, key-1);
+        Logger::instance().log("Дерево T расщеплено по ключу " + std::to_string(key-1) + ":\n");
+        Logger::instance().log("Левая половинка L: \n");
+        printTree(pair.first, nullptr, false);
+        Logger::instance().log("Правая половинка R: \n");
+        printTree(pair.second, nullptr, false);
+
+        TreapPair pairR = split(pair.second, key);
+        Logger::instance().log("Правая половинка расщеплена по ключу " + std::to_string(key-1) + ":\n");
+        Logger::instance().log("Её левая половинка M: \n");
+        printTree(pairR.first, nullptr, false);
+        Logger::instance().log("Её правая половинка R': \n");
+        printTree(pairR.second, nullptr, false);
+
+        T = merge(pair.first, pairR.second);
+        Logger::instance().log("Левая половинка L сцеплена с правой половинкой правого поддерева R' (исключили M):\n");
+        printTree(T, nullptr, false);
+
+        Logger::instance().log("Удаление элемента завершено.\n");
+        return T;
+    }
+
+    void print(const TreapPtr& T) {
+        if (T == nullptr) return;
+        Logger::instance().log("x=" + std::to_string(T->key) + "; y=" + std::to_string(T->priority) + /*"; c=" + std::to_string(count) + */"\n");
+        print(T->left);
+        print(T->right);
+    }
+
+
+
+
+
+    void showTrunks(Trunk *p)
+    {
+        if (p == nullptr) return;
+        showTrunks(p->prev);
+        Logger::instance().log(p->str);
+    }
+
+    void printTree(TreapPtr root, Trunk *prev, bool isLeft)
+    {
+        if (root == nullptr) return;
+
+        std::string prev_str = "    ";
+        Trunk *trunk = new Trunk(prev, prev_str);
+        printTree(root->left, trunk, true);
+
+        if (!prev)
+            trunk->str = "---";
+        else if (isLeft)
+        {
+            trunk->str = ".---";
+            prev_str = "   |";
+        }
+        else
+        {
+            trunk->str = "`---";
+            prev->str = prev_str;
+        }
+
+        showTrunks(trunk);
+        Logger::instance().log(std::to_string(root->key) + "\n");
+
+        if (prev)
+            prev->str = prev_str;
+        trunk->str = "   |";
+
+        printTree(root->right, trunk, false);
+
+    }
+
+}
---
+++ Pribylov/cw/src/Treap.h
@@ -0,0 +1,59 @@
+#ifndef TREAP_TREAP_H
+#define TREAP_TREAP_H
+
+#include <utility>
+#include <memory>
+#include <ctime>
+#include <vector>
+#include "Logger.h"
+
+namespace treap {
+
+    using TreapPtr = std::shared_ptr<class Treap>;
+    using TreapPair = std::pair<TreapPtr, TreapPtr>;
+
+    // Рандомизированная дерамида поиска (РДП)
+    struct Treap {
+        int key; // ключ
+        int priority; // приоритет
+        TreapPtr left;
+        TreapPtr right;
+
+        // конструктор, принимает ключ key, приоритет priority, указатели на левое и правое поддеревья left и right
+        Treap(int key, int priority = rand(), TreapPtr left = nullptr, TreapPtr right = nullptr);
+
+    };
+
+    // принимает два дерева L и R, сливает их в одно и возвращает его
+    TreapPtr merge(const TreapPtr& L, const TreapPtr& R);
+
+    // принимает дерево T и по ключу key расщепляет его, возвращает пару из левой и правой половинки
+    TreapPair split(const TreapPtr& T, int key);
+
+    // принимает дерево T и вставяет элемент с ключом key в него, возвращает это же дерево
+    TreapPtr insert(TreapPtr& T, int key);
+
+    // принимает дерево T и удаляет элемент с ключом key из него, возвращает это же дерево
+    TreapPtr remove(TreapPtr& T, int key);
+
+    // принимает вектор ключей keys и строит на их основе РДП
+    TreapPtr build(const std::vector<int>& keys);
+
+
+    // вспомогательная структура и набор функций для печати дерева
+    struct Trunk
+    {
+        Trunk *prev;
+        std::string str;
+
+        Trunk(Trunk *prev, std::string str)
+        {
+            this->prev = prev;
+            this->str = str;
+        }
+    };
+    void showTrunks(Trunk *p);
+    void printTree(TreapPtr root, Trunk *prev, bool isLeft);
+}
+
+#endif //TREAP_TREAP_H
